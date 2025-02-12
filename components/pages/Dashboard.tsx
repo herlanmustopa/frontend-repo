71aa59884737d863839ec2f8cffcfeceff3455b2
Herlan Mustopa <hrmustopa.dev@gmail.com>
2025-02-13 00:11:22 +0700

    Add Register

--- components/pages/Dashboard.tsx
+++ components/pages/Dashboard.tsx
@@ -1,13 +1,9 @@
 "use client";
-
-import {  Container } from "@mui/material";
 import Table from "@/components/organism/Table";
 
 const  DashboardPage = () => {
   return (
-      <Container>
         <Table />
-      </Container>
   );
 }
 
